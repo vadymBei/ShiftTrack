aca01642b8d289e617ab74c4a18f90d34bd97424
Vadym Bei <bey1705@gmail.com>
2025-04-11 14:54:51 +0300

    fixup! Role, EmployeeRole, EmployeeRoleUnit, EmployeeRoleUnitDepartment entities were added

---
+++ src/Services/ShiftTrack/ShiftTrack.Core/Infrastructure/Migrations/20250411115205_SYS_USR_EmployeeRoles_Entities_Added.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ShiftTrack.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SYS_USR_EmployeeRoles_Entities_Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeRoles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Scope = table.Column<int>(type: "integer", nullable: false),
+                    EmployeeId = table.Column<long>(type: "bigint", nullable: true),
+                    RoleId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeRoles_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_EmployeeRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeRoleUnits",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Scope = table.Column<int>(type: "integer", nullable: false),
+                    EmployeeRoleId = table.Column<long>(type: "bigint", nullable: true),
+                    UnitId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeRoleUnits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeRoleUnits_EmployeeRoles_EmployeeRoleId",
+                        column: x => x.EmployeeRoleId,
+                        principalTable: "EmployeeRoles",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_EmployeeRoleUnits_Units_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Units",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeRoleUnitDepartments",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EmployeeRoleUnitId = table.Column<long>(type: "bigint", nullable: true),
+                    DepartmentId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeRoleUnitDepartments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeRoleUnitDepartments_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_EmployeeRoleUnitDepartments_EmployeeRoleUnits_EmployeeRoleU~",
+                        column: x => x.EmployeeRoleUnitId,
+                        principalTable: "EmployeeRoleUnits",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeRoles_EmployeeId",
+                table: "EmployeeRoles",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeRoles_RoleId",
+                table: "EmployeeRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeRoleUnitDepartments_DepartmentId",
+                table: "EmployeeRoleUnitDepartments",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeRoleUnitDepartments_EmployeeRoleUnitId",
+                table: "EmployeeRoleUnitDepartments",
+                column: "EmployeeRoleUnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeRoleUnits_EmployeeRoleId",
+                table: "EmployeeRoleUnits",
+                column: "EmployeeRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeRoleUnits_UnitId",
+                table: "EmployeeRoleUnits",
+                column: "UnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Name",
+                table: "Roles",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeRoleUnitDepartments");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeRoleUnits");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeRoles");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
