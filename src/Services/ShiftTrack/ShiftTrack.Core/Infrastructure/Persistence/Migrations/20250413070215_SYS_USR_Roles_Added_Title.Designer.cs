1289874d8095f51458953e3993574f05a9d2f4e4
Vadym Bei <bey1705@gmail.com>
2025-04-13 10:03:00 +0300

    Title prop added to Role entity

--- src/Services/ShiftTrack/ShiftTrack.Core/Infrastructure/Persistence/Migrations/20250413070215_SYS_USR_Roles_Added_Title.Designer.cs
+++ src/Services/ShiftTrack/ShiftTrack.Core/Infrastructure/Persistence/Migrations/20250413070215_SYS_USR_Roles_Added_Title.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShiftTrack.Core.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using ShiftTrack.Core.Infrastructure.Persistence;
 namespace ShiftTrack.Core.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250413070215_SYS_USR_Roles_Added_Title")]
+    partial class SYS_USR_Roles_Added_Title
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
