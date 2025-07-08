43568d412cbeb0b2413d45fef7cbcbb9cfc623dc
Vadym Bei <bey1705@gmail.com>
2025-07-08 14:31:59 +0300

    Simplify `Mediator` implementation by consolidating pipeline handling logic and removing redundant methods.

--- src/Common/Kernel/ShiftTrack.Kernel.CQRS/Implementations/Mediator.cs
+++ src/Common/Kernel/ShiftTrack.Kernel.CQRS/Implementations/Mediator.cs
@@ -20,22 +20,28 @@ public class Mediator(IServiceProvider serviceProvider) : IMediator
 
         if (!pipelines.Any())
         {
-            return await InvokeHandler<TResponse>(
-                handler,
-                handleMethod,
-                request,
-                cancellationToken);
+            return await (Task<TResponse>)handleMethod.Invoke(handler, new object[] { request, cancellationToken });
         }
 
-        return await BuildAndExecutePipeline<TResponse>(
-            request,
-            handler,
-            handleMethod,
-            pipelines,
-            pipelineMethod,
-            cancellationToken);
-    }
+        RequestHandlerDelegate<TResponse> baseHandler = () =>
+            (Task<TResponse>)handleMethod.Invoke(handler, new object[] { request, cancellationToken });
 
+        var pipeline = pipelines
+            .Aggregate(
+                baseHandler,
+                (next, pipe) =>
+                {
+                    var currentNext = next;
+
+                    return new RequestHandlerDelegate<TResponse>(() =>
+                        (Task<TResponse>)pipelineMethod.Invoke(
+                            pipe,
+                            new object[] { request, currentNext, cancellationToken }));
+                });
+
+        return await pipeline();
+    }
+    
     public async Task Invoke(IRequest request, CancellationToken cancellationToken = default)
     {
         ArgumentNullException.ThrowIfNull(request);
@@ -47,27 +53,35 @@ public class Mediator(IServiceProvider serviceProvider) : IMediator
 
         if (!pipelines.Any())
         {
-            await InvokeHandler(
-                handler,
-                handleMethod,
-                request,
-                cancellationToken);
+            await (Task)handleMethod.Invoke(handler, new object[] { request, cancellationToken });
 
             return;
         }
 
-        await BuildAndExecutePipeline(
-            request,
-            handler,
-            handleMethod,
-            pipelines,
-            pipelineMethod,
-            cancellationToken);
+        RequestHandlerDelegate baseHandler = () =>
+            (Task)handleMethod.Invoke(handler, new object[] { request, cancellationToken });
+
+        var pipeline = pipelines
+            .Aggregate(
+                baseHandler,
+                (next, pipe) =>
+                {
+                    var currentNext = next;
+                    return new RequestHandlerDelegate(() =>
+                        (Task)pipelineMethod.Invoke(
+                            pipe,
+                            new object[] { request, currentNext, cancellationToken }));
+                });
+
+        await pipeline();
     }
 
     private (object Handler, MethodInfo HandleMethod) GetHandler(Type requestType, Type responseType)
     {
-        var handlerType = GetHandlerType(requestType, responseType);
+        var handlerType = responseType == typeof(void)
+            ? typeof(IRequestHandler<>).MakeGenericType(requestType)
+            : typeof(IRequestHandler<,>).MakeGenericType(requestType, responseType);
+        
         var handler = serviceProvider.GetRequiredService(handlerType);
 
         var handleMethod = handlerType.GetMethod(nameof(IRequestHandler<IRequest>.Handle))
@@ -95,86 +109,7 @@ public class Mediator(IServiceProvider serviceProvider) : IMediator
 
         return (pipelines, pipelineMethod);
     }
-
-    private static async Task<TResponse> BuildAndExecutePipeline<TResponse>(
-        object request,
-        object handler,
-        MethodInfo handleMethod,
-        object[] pipelines,
-        MethodInfo pipelineMethod,
-        CancellationToken cancellationToken)
-    {
-        RequestHandlerDelegate<TResponse> baseHandler = () =>
-            (Task<TResponse>)handleMethod.Invoke(handler, new[] { request, cancellationToken });
-
-        var pipeline = pipelines
-            .Aggregate(
-                baseHandler,
-                (next, pipe) =>
-                {
-                    var currentNext = next;
-
-                    return new RequestHandlerDelegate<TResponse>(() =>
-                        (Task<TResponse>)pipelineMethod.Invoke(
-                            pipe,
-                            new object[] { request, currentNext, cancellationToken }));
-                });
-
-        return await pipeline();
-    }
-
-    private static async Task BuildAndExecutePipeline(
-        object request,
-        object handler,
-        MethodInfo handleMethod,
-        object[] pipelines,
-        MethodInfo pipelineMethod,
-        CancellationToken cancellationToken)
-    {
-        RequestHandlerDelegate baseHandler = () =>
-            (Task)handleMethod.Invoke(handler, new[] { request, cancellationToken });
-
-        var pipeline = pipelines
-            .Aggregate(
-                baseHandler,
-                (next, pipe) =>
-                {
-                    var currentNext = next;
-                    return new RequestHandlerDelegate(() =>
-                        (Task)pipelineMethod.Invoke(
-                            pipe,
-                            new object[] { request, currentNext, cancellationToken }));
-                });
-
-        await pipeline();
-    }
-
-    private static async Task<TResponse> InvokeHandler<TResponse>(
-        object handler,
-        MethodInfo handleMethod,
-        object request,
-        CancellationToken cancellationToken)
-    {
-        return await (Task<TResponse>)handleMethod.Invoke(handler, new[] { request, cancellationToken });
-    }
-
-    private static async Task InvokeHandler(
-        object handler,
-        MethodInfo handleMethod,
-        object request,
-        CancellationToken cancellationToken)
-    {
-        await (Task)handleMethod.Invoke(handler, new[] { request, cancellationToken });
-    }
-
-    private static Type GetHandlerType(Type requestType, Type responseType) =>
-        responseType == typeof(void)
-            ? typeof(IRequestHandler<>).MakeGenericType(requestType)
-            : typeof(IRequestHandler<,>).MakeGenericType(requestType, responseType);
-
-    private static Type GetPipelineType(Type requestType, Type responseType) =>
-        typeof(IPipelineBehaviour<,>).MakeGenericType(requestType, responseType);
-
+    
     private static InvalidOperationException CreateHandleMethodNotFoundException(string typeName) =>
         new($"Handle method not found on {typeName}");
 }
