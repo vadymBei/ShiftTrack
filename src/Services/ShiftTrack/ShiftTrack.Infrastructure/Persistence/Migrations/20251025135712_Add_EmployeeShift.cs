da1eadd76bc18170076b1161d605ad205f047f59
bey17 <bey1705@gmail.com1>
2025-10-25 16:58:31 +0300

    Add EmployeeShift entity

---
+++ src/Services/ShiftTrack/ShiftTrack.Infrastructure/Persistence/Migrations/20251025135712_Add_EmployeeShift.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ShiftTrack.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_EmployeeShift : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeShifts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "interval", nullable: true),
+                    EndTime = table.Column<TimeSpan>(type: "interval", nullable: true),
+                    EmployeeId = table.Column<long>(type: "bigint", nullable: false),
+                    ShiftId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    AuthorId = table.Column<long>(type: "bigint", nullable: true),
+                    ModifierId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeShifts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeShifts_Employees_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_EmployeeShifts_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeShifts_Employees_ModifierId",
+                        column: x => x.ModifierId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_EmployeeShifts_Shifts_ShiftId",
+                        column: x => x.ShiftId,
+                        principalTable: "Shifts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeShifts_AuthorId",
+                table: "EmployeeShifts",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeShifts_Date_EmployeeId",
+                table: "EmployeeShifts",
+                columns: new[] { "Date", "EmployeeId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeShifts_EmployeeId",
+                table: "EmployeeShifts",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeShifts_ModifierId",
+                table: "EmployeeShifts",
+                column: "ModifierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeShifts_ShiftId",
+                table: "EmployeeShifts",
+                column: "ShiftId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeShifts");
+        }
+    }
+}
