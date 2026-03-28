f4e1452861b964771e947534b3e351ea8d12094b
bey17 <bey1705@gmail.com1>
2026-03-28 09:32:01 +0200

    Add architecture tests for ShiftTrack

---
+++ tests/ShiftTrack/ShiftTrack.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using ShiftTrack.Application.Common.Interfaces;
+using ShiftTrack.Domain.Common.Abstractions;
+using ShiftTrack.Infrastructure.Persistence;
+using Assembly = System.Reflection.Assembly;
+
+namespace ShiftTrack.ArchitectureTests.Abstractions;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(AuditableEntity).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(IApplicationDbContext).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+
+    protected static readonly Architecture Architecture = new ArchLoader()
+        .LoadAssemblies(
+            DomainAssembly,
+            ApplicationAssembly,
+            InfrastructureAssembly,
+            PresentationAssembly)
+        .Build();
+}
---
+++ tests/ShiftTrack/ShiftTrack.ArchitectureTests/Tests/LayerTests.cs
@@ -0,0 +1,70 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.xUnit;
+using ShiftTrack.ArchitectureTests.Abstractions;
+using Xunit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace ShiftTrack.ArchitectureTests.Tests;
+
+public class LayerTests : BaseTest
+{
+    private static readonly IObjectProvider<IType> DomainLayer =
+        Types().That().ResideInAssembly(DomainAssembly).As("Domain layer");
+
+    private static readonly IObjectProvider<IType> ApplicationLayer =
+        Types().That().ResideInAssembly(ApplicationAssembly).As("Application layer");
+
+    private static readonly IObjectProvider<IType> InfrastructureLayer =
+        Types().That().ResideInAssembly(InfrastructureAssembly).As("Infrastructure Layer");
+
+    private static readonly IObjectProvider<IType> PresentationLayer =
+        Types().That().ResideInAssembly(PresentationAssembly).As("Presentation Layer");
+
+    [Fact]
+    public void DomainLayer_ShouldNotDependOn_ApplicationLayer()
+    {
+        Types().That().Are(DomainLayer).Should()
+            .NotDependOnAny(ApplicationLayer)
+            .Check(Architecture);
+    }
+
+    [Fact]
+    public void DomainLayer_ShouldNotDependOn_InfrastructureLayer()
+    {
+        Types().That().Are(DomainLayer).Should()
+            .NotDependOnAny(InfrastructureLayer)
+            .Check(Architecture);
+    }
+
+    [Fact]
+    public void DomainLayer_ShouldNotDependOn_PresentationLayer()
+    {
+        Types().That().Are(DomainLayer).Should()
+            .NotDependOnAny(PresentationLayer)
+            .Check(Architecture);
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotDependOn_InfrastructureLayer()
+    {
+        Types().That().Are(ApplicationLayer).Should()
+            .NotDependOnAny(InfrastructureLayer)
+            .Check(Architecture);
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotDependOn_PresentationLayer()
+    {
+        Types().That().Are(ApplicationLayer).Should()
+            .NotDependOnAny(PresentationLayer)
+            .Check(Architecture);
+    }
+
+    [Fact]
+    public void InfrastructureLayer_ShouldNotDependOn_PresentationLayer()
+    {
+        Types().That().Are(InfrastructureLayer).Should()
+            .NotDependOnAny(PresentationLayer)
+            .Check(Architecture);
+    }
+}
