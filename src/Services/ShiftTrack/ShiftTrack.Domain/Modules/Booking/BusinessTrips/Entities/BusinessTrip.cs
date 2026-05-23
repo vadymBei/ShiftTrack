c401517ff2b1687a85be9693451b0f2cc3b26b56
bey17 <bey1705@gmail.com1>
2026-05-23 15:40:49 +0300

    Add BusinessTrip entity and related configurations

--- src/Services/ShiftTrack/ShiftTrack.Domain/Modules/Booking/BusinessTrips/Entities/BusinessTrip.cs
+++ src/Services/ShiftTrack/ShiftTrack.Domain/Modules/Booking/BusinessTrips/Entities/BusinessTrip.cs
@@ -1,5 +1,6 @@
 using ShiftTrack.Domain.Common.Abstractions;
 using ShiftTrack.Domain.Modules.Booking.BusinessTrips.Enums;
+using ShiftTrack.Domain.Modules.System.User.Employees.Entities;
 
 namespace ShiftTrack.Domain.Modules.Booking.BusinessTrips.Entities;
 
@@ -8,11 +9,10 @@ public class BusinessTrip : AuditableEntity
     public long Id { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public string Route { get; set; }
     public string Description { get; set; }
     public decimal EstimatedBudget { get; set; }
     public BusinessTripStatus Status { get; set; }
 
-    public List<BusinessTripParticipant> Participants { get; set; }
-    public List<BusinessTripLocation> Locations { get; set; }
+    public ICollection<Employee> Participants { get; set; }
+    public ICollection<BusinessTripLocation> Locations { get; set; }
 }
